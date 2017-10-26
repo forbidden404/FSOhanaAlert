d531ac5683e764d5738b8ddfc8bf5a2293e64ec4
Francisco Soares <francisco.ssn94@gmail.com>
2017-10-26 17:28:30 -0300

    Initial Commit

---
+++ FSOhanaAlert/FSOhanaAlert.h
@@ -0,0 +1,19 @@
+//
+//  FSOhanaAlert.h
+//  FSOhanaAlert
+//
+//  Created by Francisco Soares on 26/10/17.
+//  Copyright © 2017 Francisco Soares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FSOhanaAlert.
+FOUNDATION_EXPORT double FSOhanaAlertVersionNumber;
+
+//! Project version string for FSOhanaAlert.
+FOUNDATION_EXPORT const unsigned char FSOhanaAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSOhanaAlert/PublicHeader.h>
+
+
